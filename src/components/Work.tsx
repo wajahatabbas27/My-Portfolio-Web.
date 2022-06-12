73d27fb1b21fb7a9fde5e948a3d82f505892847d
wajahatabbas27 <wajahatabbas27@gmail.com>
2022-06-12 07:48:46 +0500

    just updated the padding and overflow-x-hidden in the index.css root of the directory

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -4,7 +4,10 @@ import RealState from "../assets/realestate.jpg";
 
 const Work = () => {
   return (
-    <div id='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+    <div
+      id='work'
+      className='w-full md:h-screen text-gray-300 bg-[#0a192f] py-8 sm:py-0'
+    >
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>
