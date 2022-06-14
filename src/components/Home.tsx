3806adaf7434bfb048e3f28f269db9128471b05c
wajahatabbas27 <wajahatabbas27@gmail.com>
2022-06-15 00:18:21 +0500

    added the padding top for mobile small screen

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,10 @@ import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
   return (
-    <div id='home' className='bg-[#0a192f] h-screen w-full  pt-32 xs:pt-0 '>
+    <div
+      id='home'
+      className='bg-[#0a192f] h-screen w-full  pt-32 xs:pt-0 pb-18 xs:pb-0 mb-0 '
+    >
       {/* Container */}
       <div className='max-w-[1000px] h-full mx-auto px-8 flex flex-col justify-center '>
         <p className='text-pink-600'>Hi, my name is</p>
